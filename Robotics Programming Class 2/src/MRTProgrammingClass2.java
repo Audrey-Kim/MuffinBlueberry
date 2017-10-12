d9ebdb351fb91d37577eef846ac5925064487fb2
IceCreamFan777 <32147698+IceCreamFan777@users.noreply.github.com>
2017-10-11 17:20:59 -0700

    Add files via upload

---
+++ Robotics Programming Class 2/src/MRTProgrammingClass2.java
@@ -0,0 +1,10 @@
+/*
+ * 
+ */
+public class MRTProgrammingClass2 {
+	int integer = 1;
+	double cancer = 2.3;
+	char b = 'b'; 
+	boolean ChocoOreo = false; 
+
+}
---
+++ Robotics Programming Class 2/src/MuffinChocoChip.java
@@ -0,0 +1,31 @@
+
+public class MuffinChocoChip {
+
+	public static void main(String[] args) {
+	
+		int integer = 5; 
+		int intege = 9; 
+		int integ = 16; 
+		
+		if (intege >= integer) {
+			 System.out.print("Aaron sucks ");
+			 
+		}
+		else if(integ >= intege) {
+				 System.out.print("Jeffrey sucks more tbh");
+				 
+		}
+		int inte = 3;  	 
+		while(inte < 6) { 
+			System.out.print("go programming ");
+			inte++; 
+		}
+		int in = 2;
+		for(in=0; in<5;in++){
+			System.out.println("Aaron sucks the most though actually");
+		}
+	}
+ } 
+ 
+	
+
